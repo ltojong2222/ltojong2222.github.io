babe6cd7815caf2187e1886ba4f7d02705a62ff5
ltojong2222 <68831885+ltojong2222@users.noreply.github.com>
2020-07-26 20:20:25 -0500

    Cipher

---
+++ SubstitutionCipher/SubstitutionCipher/Cipher.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SubstitutionCipher
+{
+    class Cipher
+    {
+
+        public static void Encrypt()
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string different = "zyxwvutsrqponmlkjihgfedcba";
+
+
+            WriteLine("Hello there agent!");
+            WriteLine("Enter lowercase text.");
+            string decipher = ReadLine();
+
+            foreach (char c in decipher)
+            {
+                int index = alphabet.IndexOf(c);
+                decipher = decipher.Replace(c, different[index]);
+            }
+            WriteLine("Your encryptid statement is: " + decipher);
+            WriteLine("Thanks for playing!");
+            WriteLine("Press ENTER to exit...");
+        }
+    }
+}
---
+++ SubstitutionCipher/SubstitutionCipher/Program.cs
@@ -0,0 +1,21 @@
+﻿//Substitution Cipher by L. Tojong
+//Intro to Programming, SU 2020
+//used janell-baxter's simple cipher optional framework as reference from canvas
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SubstitutionCipher
+{
+    class Program
+    {
+        static void Main()
+        {
+            Cipher.Encrypt();
+            ReadKey();
+        }
+    }
+}
